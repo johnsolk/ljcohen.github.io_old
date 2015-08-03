83455a68fa963c902311f6b9e50cc72d97bb54d5
Lisa Cohen <cohenl06@Lisas-MacBook-Pro.local>
2015-08-03 12:03:04 -0400

    changes to Dasen index

--- analyses/Dasen/scripts/cummeRbund_dasen_branchial_July2015.R
+++ analyses/Dasen/scripts/cummeRbund_dasen_branchial_July2015.R
@@ -1,5 +1,10 @@
 library(cummeRbund)
 library(biomaRt)
+library(gplots)
+library(RColorBrewer)
+library(biomaRt)
+library("genefilter")
+library("lattice")
 setwd("~/Documents/NYUMC/Dasen/brachial/cuffdiff_output_1")
 cuff<-readCufflinks()
 cuff
@@ -46,12 +51,42 @@ colnames(new_gene.rep.matrix)<-customnames_2
 new_isoforms<-merge(new_isoforms.rep.matrix,orig_isoforms,by="test_id")
 new_genes<-merge(new_gene.rep.matrix,orig_genes,by="test_id")
 dim(new_genes)
-new_genes_filtered<-subset(new_genes,rowSums(new_genes[,c(2:7)])>1)
-dim(new_genes_filtered)
-new_genes_padj<-subset(new_genes_filtered,new_genes$q_value<0.05)
+new_genes_padj<-subset(new_genes,new_genes$q_value<0.05)
 dim(new_genes_padj)
 new_genes_log2FCcutoff<-subset(new_genes_padj,new_genes_padj$log2.fold_change>1 | new_genes_padj$log2.fold_change< -1)
-dim(new_genes_log2FCcutoff)
+new_genes_log2FCcutoff_genenames<-subset(new_genes_log2FCcutoff,new_genes_log2FCcutoff$gene!="-")
+dim(new_genes_log2FCcutoff_genenames)
+head(new_genes_log2FCcutoff_genenames)
+#
+head(gene.rep.matrix)
+gene.rep.matrix.filtered<-subset(gene.rep.matrix,rowSums(gene.rep.matrix[,])>1)
+mean.gene.rep.matrix<-rowMeans(gene.rep.matrix.filtered)
+length(mean.gene.rep.matrix)
+gene.rep.matrix.filtered<-cbind(gene.rep.matrix.filtered,mean.gene.rep.matrix)
+head(gene.rep.matrix.filtered)
+gene.rep.matrix.filtered<-cbind(gene.rep.matrix.filtered,rownames(gene.rep.matrix.filtered))
+colnames(gene.rep.matrix.filtered)<-c("BR-C-Mutant","BR-A-Mutant","BR-B-Mutant","BR-C-Control","BR-B-Control","BR-A-Control","mean.gene.rep.matrix","gene_id")
+gene.rep.matrix.filtered_merge<-merge(gene.rep.matrix.filtered,new_genes[,c(8,16,19)],by="gene_id")
+head(gene.rep.matrix.filtered_merge)
+plot(gene.rep.matrix.filtered_merge$mean.gene.rep.matrix, gene.rep.matrix.filtered_merge$log2.fold_change.,col=ifelse(gene.rep.matrix.filtered_merge$q_value < 0.05, "red","gray67"),main="(tuxedo) Brachial Control vs. Thoracic Control",xlim=c(1,50),ylim=c(-50,50),pch=20,cex=1)
+abline(h=c(-1,1), col="blue")
 
-write.csv(new_genes, file="genes-fpkm-matrix-replicates.csv")
-write.csv(new_isoforms, file="isoforms-fpkm-matrix-replicates.csv")
+# heatmap
+d<-as.matrix(new_genes_log2FCcutoff_genenames[,c(2:7)])
+rownames(d) <- new_genes_log2FCcutoff_genenames[,9]
+hr <- hclust(as.dist(1-cor(t(d), method="pearson")), method="complete")
+mycl <- cutree(hr, h=max(hr$height/1.5))
+clusterCols <- rainbow(length(unique(mycl)))
+#myClusterSideBar <- clusterCols[mycl]
+myheatcol <- greenred(75)
+heatmap.2(d, main="(tuxedo) Brachial q_value<0.05, log2FC>1 | log2FC< -1", 
+          Rowv=as.dendrogram(hr),
+          cexRow=1,cexCol=0.8,srtCol= 90,
+          adjCol = c(NA,0),offsetCol=2.5, 
+          Colv=NA, dendrogram="row", 
+          scale="row", col=myheatcol, 
+          density.info="none", 
+          trace="none")
+
+#write.csv(new_genes, file="genes-fpkm-matrix-replicates.csv")
+#write.csv(new_isoforms, file="isoforms-fpkm-matrix-replicates.csv")
